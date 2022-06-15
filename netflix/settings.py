6a32f73be48197bfb6e7e233301f0507fe8ef5e2
Ziyodullokhon <studiovisual0077@gmail.com>
2022-06-15 16:38:31 -0400

    Fixed some problems

--- netflix/settings.py
+++ netflix/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jmo#j#a-&5812rruy4n4=!u&wynp-lrs)l=1sh*08yu^*1lm75'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
